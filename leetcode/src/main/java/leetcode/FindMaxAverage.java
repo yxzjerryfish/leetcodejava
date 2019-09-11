ef2170c23e551615b0392f14b0afbede49f24f1b
yuxz <yxzjerryfish@hotmail.com>
2019-09-11 10:08:27 +0800

    643. 子数组最大平均数 I
    好变态的用例，好多方法都超时了

--- leetcode/src/main/java/leetcode/FindMaxAverage.java
+++ leetcode/src/main/java/leetcode/FindMaxAverage.java
@@ -14,19 +14,22 @@ import java.util.LinkedList;
 public class FindMaxAverage {
     public double findMaxAverage(int[] nums, int k) {
         LinkedList<Integer> link = new LinkedList<>();
-
+        double [] sum= new double[nums.length -k +1];
         for (int i=0;i<k;i++){
             link.add(nums[i]);  //初始化
         }
 
         double flag  = finddouble(link);
+        sum[0] = flag;
         for (int i=k;i<nums.length;i++){
-            link.removeFirst();
+            int a = link.removeFirst();
             link.addLast(nums[i]);
-            flag = finddouble(link) >flag?finddouble(link):flag;
+            flag = flag - a + nums[i];
+            sum[i-k+1] = flag;
         }
 
-        return flag;
+        Arrays.sort(sum);
+        return sum[sum.length-1]/k;
     }
 
     private double finddouble(LinkedList<Integer> list){
@@ -34,7 +37,7 @@ public class FindMaxAverage {
         for (Integer l:list){
             sum+=l;
         }
-        return sum/list.size();
+        return sum;
     }
 
     public static void main(String[] args) {
