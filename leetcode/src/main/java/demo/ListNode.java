00c02b684b7e9409815e6b529698873cd12bb5da
fish <yxzjerryfish@hotmail.com>
2019-09-13 20:55:58 +0800

    所有链表类的基础节点

--- leetcode/src/main/java/demo/ListNode.java
+++ leetcode/src/main/java/demo/ListNode.java
@@ -9,7 +9,7 @@ package demo;
  * @USE :
  */
 public class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
+     public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
 }
