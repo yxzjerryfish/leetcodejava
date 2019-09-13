a261051ca9659eba8a6a4ecc7892fc53d2a022b9
fish <yxzjerryfish@hotmail.com>
2019-09-13 20:50:57 +0800

    所有链表类的基础节点

---
+++ leetcode/src/main/java/demo/ListNode.java
@@ -0,0 +1,15 @@
+package demo;
+
+/**
+ * 所有链表类的基础节点
+ *
+ * @author : Fish Paradise
+ * @version 1.0
+ * @date : 2019/9/13 20:49
+ * @USE :
+ */
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+}
