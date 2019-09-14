c759fe4f28fbf6346a4a37020bfb7fd10b4818b8
fish <yxzjerryfish@hotmail.com>
2019-09-14 20:53:09 +0800

    第二种方法

--- leetcode/src/main/java/leetcode/StrStr.java
+++ leetcode/src/main/java/leetcode/StrStr.java
@@ -14,12 +14,22 @@ public class StrStr {
 //    }
 
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        char[] h = haystack.toCharArray();
+        char [] n = needle.toCharArray();
+        if (needle.length() == 0 ) return 0;
+        if(haystack.length() < needle.length()) return -1;
+        for (int i=0;i<h.length - n.length+1;i++){
+            for (int j = 0;j<n.length;j++){
+                if (h[i+j] != n[j] ) break;
+                if (j==n.length-1) return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        String a = "hello";
-        String b = "ms";
+        String a = "a";
+        String b = "a";
         System.out.println(new StrStr().strStr(a,b));
     }
 }
