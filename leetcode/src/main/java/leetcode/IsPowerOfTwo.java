6c5efdd0a71f43d3f79c279ba26111112467e719
yuxz <yxzjerryfish@hotmail.com>
2019-09-18 10:12:12 +0800

    231. 2的幂  另一种算法

--- leetcode/src/main/java/leetcode/IsPowerOfTwo.java
+++ leetcode/src/main/java/leetcode/IsPowerOfTwo.java
@@ -8,18 +8,22 @@ package leetcode;
  * @date 2019-09-18
  */
 public class IsPowerOfTwo {
+//    public boolean isPowerOfTwo(int n) {
+//        if( n == Integer.MIN_VALUE) return false;
+//       String m = Integer.toBinaryString(n);
+//       char [] ls = m.toCharArray();
+//       Integer flag = 0;
+//       for (char l:ls){
+//           if( l == '1') flag++;
+//       }
+//       return flag == 1;
+//    }
+
     public boolean isPowerOfTwo(int n) {
-        if( n == Integer.MIN_VALUE) return false;
-       String m = Integer.toBinaryString(n);
-       char [] ls = m.toCharArray();
-       Integer flag = 0;
-       for (char l:ls){
-           if( l == '1') flag++;
-       }
-       return flag == 1;
+        return n >0 && (n&(n-1)) ==0;
     }
 
     public static void main(String[] args) {
-        System.out.println(new IsPowerOfTwo().isPowerOfTwo(-2147483648));
+        System.out.println(new IsPowerOfTwo().isPowerOfTwo(1024));
     }
 }
